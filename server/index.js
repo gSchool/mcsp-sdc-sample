b985a22543eb4e0ff583c2dae6d026768c0ba8b2
Danny Andrews <lunde.andrews@gmail.com>
2023-06-15 16:12:15 -0400

    Add single task route

--- server/index.js
+++ server/index.js
@@ -16,6 +16,14 @@ app.get("/api/tasks", (req, res) => {
   });
 });
 
+app.get("/api/tasks/:id", (req, res) => {
+  const id = req.params.id;
+
+  sql`SELECT * FROM tasks WHERE id = ${id}`.then((rows) => {
+    res.send(rows);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
